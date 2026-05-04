eb404a00616f4d25f47e7cb0bd781a874b36efe4
UdaraDewshan <dewudara@gmail.com>
2026-05-05 01:06:48 +0530

    add update and delete methods to ProjectController; implement ID handling in ProjectService and use Lombok in ProjectDto

--- src/main/java/com/decodelabs/dreambuilds/dto/ProjectDto.java
+++ src/main/java/com/decodelabs/dreambuilds/dto/ProjectDto.java
@@ -3,10 +3,16 @@ package com.decodelabs.dreambuilds.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ProjectDto {
 
+    private int id;
 
     @NotBlank(message = "Project title cannot be empty")
     private String title;
@@ -15,9 +21,4 @@ public class ProjectDto {
     @Min(value = 1, message = "Budget must be greater than zero")
     private Double budget;
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public Double getBudget() { return budget; }
-    public void setBudget(Double budget) { this.budget = budget; }
 }
